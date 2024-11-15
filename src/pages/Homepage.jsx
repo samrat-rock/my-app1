81978ff3fc5facd769889692d4653decd5d26ab8
samrat-rock <samratpandey715@gmail.com>
2024-11-15 11:40:35 +0545

    myapp

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Homepage() {
+  return <h2>Welcome to our Shop!</h2>;
+}
+
+export default Homepage;
---
+++ src/pages/Productpage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Productpage() {
+  return <h2>Product Details</h2>;
+}
+
+export default Productpage;
